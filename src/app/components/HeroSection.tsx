5b8da98bbb68aab74c16e3f8c9e222430700e33d
riyu.iring <oribelloruthiemy@gmail.com>
2026-09-08 19:59:09 +0800

    update

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -44,7 +44,7 @@ export function HeroSection() {
                 duration: 0.6,
                 ease: "easeOut",
             }}
-            className="heroOverlay absolute inset-0 px-6  flex flex-col gap-8 items-center justify-end pb-18">
+            className="heroOverlay absolute inset-0 px-6  flex flex-col gap-8 items-center justify-end pb-24 md:pb-18">
             <div className="flex flex-col gap-3 text-secondary ">
                 <div className="flex flex-col font-cormorant gap-1 items-center font-semibold text-4xl md:text-5xl uppercase text-shadow-md">
                     <MaskedText delay={0.4}>
